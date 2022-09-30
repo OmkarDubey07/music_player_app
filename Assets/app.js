0864651ee99588034a9699fe2c76bcb7acd917bd
omkar nath dubey <omkardubey1530@gmail.com>
2022-09-30 13:43:33 +0530

    commit all files

---
+++ Assets/app.js
@@ -0,0 +1,18 @@
+const carousel = [...document.querySelectorAll(".carousel img")];
+const carouselImageIndex = 0;
+
+const changeCarousel = () => {
+  carousel[carouselImageIndex].classList.toggle('active');
+
+  if (carouselImageIndex >   carousel.length -1) {
+    carouselImageIndex = 0;
+  } else {
+    carouselImageIndex++;
+  }
+
+  carousel[carouselImageIndex].classList.toggle('active');
+};
+
+setInterval(() => {
+  changeCarousel();
+}, 3000);
