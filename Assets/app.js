867c20a3f875705cd733ffbed9d5dcfc04bca0a2
omkar nath dubey <omkardubey1530@gmail.com>
2022-10-04 18:49:30 +0530

    commit it

--- Assets/app.js
+++ Assets/app.js
@@ -16,3 +16,43 @@ const changeCarousel = () => {
 setInterval(() => {
   changeCarousel();
 }, 3000);
+
+// navigation
+
+// toggling music player
+
+const musicPlayerSection = document.querySelector('.music-player-section');
+
+let clickCount = 1;
+
+musicPlayerSection.addEventListener('click', () => {
+    if(clickCount >= 2){
+        musicPlayerSection.classList.add('active');
+        clickCount = 1;
+        return;
+    }
+    clickCount++;
+    setTimeout(() => {
+        clickCount = 1;
+    }, 250);
+});
+
+// back from music player 
+
+const backToHomeBtn = document.querySelector('.music-player-section .back-btn');
+
+backToHomeBtn.addEventListener('click' , () => {
+  musicPlayerSection.classList.remove('active');
+});
+
+//   now access the playlists
+
+const playlistSection = document.querySelector('.playlist');
+const navBtn = document.querySelector('.music-player-section .nav-btn');
+
+navBtn.addEventListener('click', () => {
+    playlistSection.classList.add('active');
+});
+
+// back from playlist to music player
+
