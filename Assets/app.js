45b0d7d6870d565b9b9cd560cba83e3dfa631de7
omkar nath dubey <omkardubey1530@gmail.com>
2022-10-05 23:03:46 +0530

    commit it

--- Assets/app.js
+++ Assets/app.js
@@ -115,4 +115,21 @@ const setMusic = (i) => {
 
 setMusic(0);
 
+// format duration in 00 : 00 format
+
+const formatTime = (time) =>{
+  let min = Math.floor(time / 60);
+  if(min < 10){
+    min = `0` + min ;
+  }
+
+  let sec = Math.floor(time % 60);
+  if(sec < 10){
+    sec = `0` + sec ;
+  }
+  return `${min} : ${sec}` ;
+}
+
+// now we'll add play/pause event
+
 
